63c321cf599689b4e6c8741620adf1fad3ca7f16
munnik <martijndemunnik@protonmail.com>
2023-07-25 21:37:30 +0200

    Test for LongestStraight

--- shape/shape_test.go
+++ shape/shape_test.go
@@ -19,3 +19,10 @@ func TestCmp(t *testing.T) {
 		t.Fatalf("Expected two equal shapes but got %v and %v", s1, s2)
 	}
 }
+
+func TestLongestStraight(t *testing.T) {
+	s1 := NewShape().MustAddCube(&Coord{1, 0, 0}).MustAddCube(&Coord{2, 0, 0}).WithSmallestScore()
+	if s1.LongestStraight() != 3 {
+		t.Fatalf("Expected longest straight 3 but got %v", s1.LongestStraight())
+	}
+}
